74c979ba8d997e553e168887061479165651bd7d
Emiliano A. Billi <emiliano.billi@gmail.com>
2024-10-16 16:09:25 -0300

    Update table.go

--- table.go
+++ table.go
@@ -18,6 +18,9 @@ func (r *RouteTable) SetDestination(ip net.IP, vaddr uint16) {
 	r.table[ipb] = vaddr
 }
 func (r *RouteTable) GetDestination(ip net.IP) (uint16, bool) {
+	if r.table == nil {
+		r.table = make(map[uint32]uint16)
+	}
 	vaddr, ok := r.table[binary.BigEndian.Uint32(ip.To4())]
 	return vaddr, ok
 }
